be0a2722fd9468635b9a45481cd5124c01395532
Tashvik Srivastava <tashvik.12112279@lpu.in>
2024-04-28 01:57:02 +0530

    host update

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "snuggle-sigma.vercel.app";
+export const host = "https://chat-application-kohl-gamma.vercel.app";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
